24077b65608b8982f46491f43b2381deba5d13b0
githhub1234567890 <90086385+githhub1234567890@users.noreply.github.com>
2023-02-26 15:03:59 +0000

    Add files via upload

---
+++ Java.js
@@ -0,0 +1,17 @@
+function nextpage()
+{
+    console.log("y")
+    window.location="index1.html"
+}
+function ODPage()
+{
+    window.location="ODpage.html";
+}
+function MDPage()
+{
+    window.location="MDpage.html"
+}
+function SSDPage()
+{
+    window.location="SSDpage.html"
+}
